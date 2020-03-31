b00d09f40b1d770fabff889a2229af6eb4843708
nocaed <codenamedeacon@outlook.com>
2020-03-31 14:58:13 -0400

    formatting

--- src/sample/Pizza.java
+++ src/sample/Pizza.java
@@ -57,7 +57,7 @@ public abstract class Pizza {
 
         // appends all toppings to strToppings
         for(String topping : toppings) {
-            strToppings += topping + ", ";
+            strToppings += topping.toLowerCase() + ", ";
             numToppings++;
         }
 
