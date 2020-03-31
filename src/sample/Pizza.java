362c93983df10761d830bff53bdd16b829d1c903
nocaed <codenamedeacon@outlook.com>
2020-03-31 13:07:35 -0400

    Finished writing Pizza, still need to do toString

--- src/sample/Pizza.java
+++ src/sample/Pizza.java
@@ -6,5 +6,30 @@
 
 package sample;
 
-public class Pizza {
+import java.util.ArrayList;
+
+public abstract class Pizza {
+    protected String style;
+    protected String size;
+    protected ArrayList<String> toppings;
+
+    public Pizza(String style, String size, ArrayList<String> toppings) {
+        this.style = style;
+        this.size = size;
+        // TODO: this is a shallow copy, maybe do a deep copy
+        this.toppings = (ArrayList<String>) toppings.clone();
+    }
+
+    public Pizza(String style, String size) {
+        this.style = style;
+        this.size = size;
+        this.toppings = new ArrayList<>();
+    }
+
+    public abstract int pizzaPrice();
+
+    public String toString() {
+        // TODO: actually return a formatted string, idk what it should look like tho
+        return "";
+    }
 }
