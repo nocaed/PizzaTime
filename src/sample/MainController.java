82028f292967e84933706112033be42c001b2d79
nocaed <codenamedeacon@outlook.com>
2020-04-01 17:28:48 -0400

    Connected Controller to GUI

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -6,5 +6,41 @@
 
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class MainController {
+    @FXML
+    private URL location;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private Button addBtn,
+                   removeBtn,
+                   clearBtn,
+                   addOrderBtn,
+                   showBtn;
+
+    @FXML
+    private ComboBox<String> styleCombo,
+                             sizeCombo;
+
+    @FXML
+    private ListView<String> toppingsList,
+                             selectedList;
+
+    @FXML
+    private ImageView pizzaImage;
+
+    public MainController() {}
+
+
 }
