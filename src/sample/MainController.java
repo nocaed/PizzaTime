d4c12cfe220b4ce0b6271ae0f56257ff25fc6f01
nocaed <codenamedeacon@outlook.com>
2020-04-01 17:48:40 -0400

    Linked the two GUIs together

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -7,12 +7,20 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class MainController {
@@ -40,7 +48,37 @@ public class MainController {
     @FXML
     private ImageView pizzaImage;
 
-    public MainController() {}
+    @FXML
+    private TextArea outputTA;
+
+    private ArrayList<String> toppings;
+
+    private ArrayList<Pizza> order;
+
+    public MainController() {
+        toppings = new ArrayList<>();
+        order = new ArrayList<>();
+        // TODO load in options for comboboxes
+        // TODO load in toppingsList options
+    }
+
+    @FXML
+    private void loadOrderScreen() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("detailsscreen.fxml"));
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene((Pane) loader.load()));
+
+            DetailController controller = loader.<DetailController>getController();
+            controller.init(order);
+
+            stage.show();
+        }
+        catch(IOException e) {
+            outputTA.setText(e.toString());
+        }
+    }
 
 
 }
