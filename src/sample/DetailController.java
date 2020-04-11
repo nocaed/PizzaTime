3aa69ed644010eddd6238dfd0b1706097b328c4c
nocaed <codenamedeacon@outlook.com>
2020-04-10 20:16:25 -0400

    Added total cost of order

--- src/sample/DetailController.java
+++ src/sample/DetailController.java
@@ -8,26 +8,24 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
+import java.text.DecimalFormat;
 
 public class DetailController {
-    @FXML
-    private URL location;
-
-    @FXML
-    private ResourceBundle resources;
-
     @FXML
     private TextArea orderTA;
 
     @FXML
     private Button closeBtn,
+    
                    clearBtn;
+
+    @FXML
+    private Label priceLbl;
 
     /**
      * Initializes the order view to show all pizzas in the order.
@@ -36,10 +34,15 @@ public class DetailController {
      */
     void init(ArrayList<Pizza> pizzas) {
         String output = "";
+        double total = 0.0;
+        double currPrice;
         for(Pizza pizza : pizzas) {
-            output += pizza.toString() + " Cost: $" + pizza.pizzaPrice() + "\n";
+            currPrice = pizza.pizzaPrice();
+            output += pizza.toString() + " Cost: $" + currPrice + "\n";
+            total += currPrice;
         }
         orderTA.setText(output);
+        priceLbl.setText(new DecimalFormat("$#,##0.00").format(total));
     }
 
     @FXML
